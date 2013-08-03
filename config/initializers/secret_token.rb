a19cde4d376e744d904e26db676dbd76ae9e2287
Kendall Aubertot <kendall@chronosinteractive.com>
2013-08-03 16:44:16 -0600

    Initialize Rails
    Initialize Rails with latest version (4.0.0 w00t!). Do not include unit tests,
    but include rspec instead.
    
    Beginning to address #6364960125218

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RdioAutocacher::Application.config.secret_key_base = 'd7b8923ee8afba700a885b4dc20d5e1b20e597617fd9118d5ddf1e4700d70c7db78947f13b5184718d2bb722bf7b12c8cbc7c9263d63472db50c72796fc56669'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RdioAutocacher::Application.config.session_store :cookie_store, key: '_rdio-autocacher_session'
